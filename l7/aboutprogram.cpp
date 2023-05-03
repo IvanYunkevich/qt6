93d6bb721d3b9b92318e22bd8e5f5932f5cdbf9c
IvanYunkevich <ivan01089@mail.ru>
2023-05-03 14:42:22 +0300

    l7

---
+++ l7/aboutprogram.cpp
@@ -0,0 +1,40 @@
+#include "aboutprogram.h"
+#include "ui_aboutprogram.h"
+#include <QIcon>
+
+aboutProgram::aboutProgram(QDialog *parent) :
+    QDialog(parent),
+    ui(new Ui::aboutProgram)
+{
+    ui->setupUi(this);
+
+
+    setDefaultLanguage();
+}
+
+aboutProgram::~aboutProgram()
+{
+    delete ui;
+}
+
+void aboutProgram::setDefaultLanguage()
+{
+    this->setWindowTitle(tr("О программе"));
+
+    ui->label_info->setText(tr("Блокнот"));
+}
+
+void aboutProgram::setLanguage(Language::CodeLanguage code)
+{
+    if(Language::CodeLanguage::en == code)
+    {
+        qtranslator.load(":/language/qm/QtLanguage_en.qm");
+        qApp->installTranslator(&qtranslator);
+        ui->retranslateUi(this);
+    }else
+    {
+        qtranslator.load(":/language/qm/QtLanguage_ru.qm");
+        qApp->installTranslator(&qtranslator);
+        ui->retranslateUi(this);
+    }
+}
---
+++ l7/comb.cpp
@@ -0,0 +1,66 @@
+#include "comb.h"
+#include "ui_comb.h"
+
+buttonChange::buttonChange(QDialog *parent) :
+    QDialog(parent),
+    ui(new Ui::buttonChange)
+{
+    ui->setupUi(this);
+
+    ui->radioButton_ctrl_n->setChecked(true);
+    ui->radioButton_ctrl_o->setChecked(true);
+    ui->radioButton_ctrl_s->setChecked(true);
+    ui->radioButton_ctrl_q->setChecked(true);
+
+    setDefaultLanguage();
+}
+
+buttonChange::~buttonChange()
+{
+    delete ui;
+}
+
+bool buttonChange::getCheckOpen()
+{
+    return ui->radioButton_ctrl_o->isChecked();
+}
+
+bool buttonChange::getCheckSave()
+{
+    return ui->radioButton_ctrl_s->isChecked();
+}
+
+bool buttonChange::getCheckNew()
+{
+    return ui->radioButton_ctrl_n->isChecked();
+}
+
+bool buttonChange::getCheckQuit()
+{
+    return ui->radioButton_ctrl_q->isChecked();
+}
+
+void buttonChange::setDefaultLanguage()
+{
+    this->setWindowTitle(tr("Кнопки"));
+
+    ui->labelCreate->setText(tr("Создать"));
+    ui->labelOpen->setText(tr("Открыть"));
+    ui->labelSave->setText(tr("Сохранить"));
+    ui->labelQuit->setText(tr("Выход"));
+}
+
+void buttonChange::setLanguage(Language::CodeLanguage code)
+{
+    if(Language::CodeLanguage::en == code)
+    {
+        qtranslator.load(":/language/qm/QtLanguage_en.qm");
+        qApp->installTranslator(&qtranslator);
+        ui->retranslateUi(this);
+    }else
+    {
+        qtranslator.load(":/language/qm/QtLanguage_ru.qm");
+        qApp->installTranslator(&qtranslator);
+        ui->retranslateUi(this);
+    }
+}
